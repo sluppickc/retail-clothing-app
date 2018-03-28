3330cf077a1ec7aa10d556a373fb25b11111e46e
Chris Sluppick <sluppickc@gmail.com>
2018-03-28 15:38:18 -0500

    Table view and prototype cell

--- retail-clothing-app/retail-clothing-app/Controller/CategoriesVC.swift
+++ retail-clothing-app/retail-clothing-app/Controller/CategoriesVC.swift
@@ -8,7 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CategoriesVC: UIViewController {
+    
+    @IBOutlet weak var categoryTable: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ retail-clothing-app/retail-clothing-app/View/CategoryCell.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryCell.swift
+//  retail-clothing-app
+//
+//  Created by Chris Sluppick on 3/28/18.
+//  Copyright © 2018 Chris Sluppick. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
